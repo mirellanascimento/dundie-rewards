822e6ac7abbe8bbdd485b1368ad933bd81aef1a6
Mirella Nascimento <mirellagsn@gmail.com>
2025-12-16 23:43:14 -0300

    Set up project initial organization and metadaTA
    - Add setup.py to turn installable
    - Add __main__ for executable
    - Add __init__ for packages
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    packages=find_packages(),
+)
