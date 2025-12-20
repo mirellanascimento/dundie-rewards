bffcb90c3f0f2544363a3b4b0356a2e66ff4eb62
Mirella Nascimento <mirellagsn@gmail.com>
2025-12-20 00:09:37 -0300

    Add CI GHA

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     description="Reward Point System for Dunder Mifflin",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
+    python_requires=">=3.8",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
